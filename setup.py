6162353d4b0c4e431322d0332a535c92dafe4ad1
Monish Sabharwal <monishsabharwal77@gmail.com>
2025-04-19 00:39:19 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Insurance Claim AI Project',
+    version = '0.0.0',
+    author = 'Monish Sabharwal',
+    author_email = 'monishsabharwal77@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
